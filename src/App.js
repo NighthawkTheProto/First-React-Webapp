58397ee65c324b8c6974122e38fbd70a8b9c8632
NighthawkTheProto <83007809+NighthawkTheProto@users.noreply.github.com>
2022-11-04 22:34:03 +0000

    Possibly fixed app startup not showing homepage

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
       <Router>
       <Navbar />
       <Routes>
-        <Route path='/home' exact element={<Home/>} />
+        <Route path='/' exact element={<Home/>} />
         <Route path='/home' element={<Home/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/privacy' element={<Privacy/>} />
